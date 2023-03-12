c9862d1a549496faa6a09d08e45f2d9a91a68cb7
Igor Velmud Bandero <igorbandero@gmail.com>
2023-03-12 14:32:59 -0300

    Layout implementado

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import HomePage from "./pages/HomePage/HomePage"
 import SeatsPage from "./pages/SeatsPage/SeatsPage"
 import SessionsPage from "./pages/SessionsPage/SessionsPage"
 import SuccessPage from "./pages/SuccessPage/SuccessPage"
+import axios from 'axios';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 
 export default function App() {
-    return (
-        <>
-           <NavContainer>CINEFLEX</NavContainer>
 
-            {/* <HomePage /> */}
-            {/* <SeatsPage /> */}
-            <SessionsPage />
-            {/* <SuccessPage /> */}
-        </>
+       return (
+        <BrowserRouter>
+           <NavContainer>CINEFLEX</NavContainer>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/sessoes" element={<SessionsPage />} />
+                <Route path="/seats" element={<SeatsPage />} />
+                <Route path="/success" element={<SuccessPage />} />
+            </Routes>            
+        </BrowserRouter>
     )
 }
 
